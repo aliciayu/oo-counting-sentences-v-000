b185a00747a81d4f61aa87eba0dac2d190c13bdd
Alicia Yu <alicia.yu.16@gmail.com>
2017-11-06 22:31:29 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,24 +3,15 @@ require 'pry'
 class String
 
   def sentence?
-    "".end_with?(".")
-    return true
-  else
-    return false
+    self.end_with?(".")
   end
 
   def question?
-    "".end_with?("?")
-    return true
-  else
-    return false
+    self.end_with?("?")
   end
 
   def exclamation?
-    "".end_with?("!")
-    return true
-  else
-    return false
+    self.end_with?("!")
   end
 
   def count_sentences
